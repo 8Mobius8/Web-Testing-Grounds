9562c8feafe26b17bfcb85f4a28f4575bd41bae3
Mark Odell <dracone.cordis@gmail.com>
2014-10-19 20:58:45 -0700

    Some inital pesudo and vars I think I'll need.

--- canvas.js
+++ canvas.js
@@ -1,9 +1,23 @@
 /*
-	Mark Odell : Simple Canvas Starter js
+	Mark Odell : Drawing Interactive Triangles
+    The purpose of this assignment is to be able to interactively draw rectangle shapes with rubberbanding onto a "graph paper" canvas. We use the term rubberbanding for when refering to when a mouse click is up or down.
 */
 var canvas, ctx;
 var FULLSCREEN = false;
+var REC_COLOR, GRAPH_COLOR; 
 
+// Need two listeners: Mouse Down, Mouse Up
+// Mouse Down()
+//      Save point where mouse is currently
+// Mouse Up()
+//      Save point where mouse is currently
+//      Use previously saved point and poiny from Mouse Down to draw a rect
+
+// ExtraC: Add UI somewhere that give coordinates of where
+// mouse is currently, regards of being clicked.
+
+
+/* Some Setup functions I use to make the page look nice-er */
 // Setup function for loading the page
 window.onload = function () {
 	initalize();
