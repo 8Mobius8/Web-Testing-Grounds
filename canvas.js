54d35063b95da28bf63843c458704d31e38f76df
Mark Odell <dracone.cordis@gmail.com>
2014-10-19 23:10:49 -0700

    Added rubberbanding effect. SUPER easy for just mouse. Pretty fun. Should as a UI box later.

--- canvas.js
+++ canvas.js
@@ -3,10 +3,11 @@
     The purpose of this assignment is to be able to interactively draw rectangle shapes with rubberbanding onto a "graph paper" canvas. We use the term rubberbanding for when refering to when a mouse click is up or down.
 */
 var FULLSCREEN = false;
-var REC_COLOR,
+var REC_FILL_COLOR = "rgba(255, 0, 0, 0.4)",
+    REC_STROKE_COLOR = "rgb(255, 0, 0)",
     GRAPH_COLOR = "paleTurquoise",
     GRAPH_UNITS = 20; 
-var canvas, ctx, firstPt, secondPt, lastRect;
+var canvas, ctx, firstPt, secondPt, lastRect, lastImg, isMouseDown;
 
 // Setup function for loading the page
 window.onload = function () {
@@ -14,28 +15,47 @@ window.onload = function () {
     draw_GraphPaper();
     canvas.addEventListener("mousedown", do_mouseDown);
     canvas.addEventListener("mouseup", do_mouseUp);
+    canvas.addEventListener("mousemove", do_mouseAt);
 
 };
 
-// Need two listeners: Mouse Down, Mouse Up
+// Need three listeners: Mouse Down, Mouse Up
 // Mouse Down()
 function do_mouseDown(event) {
     // Save point where mouse is currently
     firstPt = { x:event.clientX - canvas.offsetLeft,
                 y:event.clientY - canvas.offsetTop  };
+    // Save image
+    lastImg = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    isMouseDown = true;
 }
 
 // Mouse Up()
 function do_mouseUp(event){
     // Save point where mouse is currently
-    secondPt = { x:event.clientX - canvas.offsetLeft,
-                 y:event.clientY - canvas.offsetTop  };
+    // secondPt = { x:event.clientX - canvas.offsetLeft,
+    //              y:event.clientY - canvas.offsetTop  };
     // Use previously saved point and point from Mouse Down to draw a rect
-    lastRect = { x: firstPt.x,
-                 y: firstPt.y,
-                 w: secondPt.x - firstPt.x,
-                 h: secondPt.y - firstPt.y };
-    draw_aRect(REC_COLOR, lastRect);
+    // lastRect = { x: firstPt.x,
+    //              y: firstPt.y,
+    //              w: secondPt.x - firstPt.x,
+    //              h: secondPt.y - firstPt.y };
+    //draw_aRect(null, null, lastRect);
+    isMouseDown = false;
+}
+
+// Mouse At()  Executes the mouse moves
+function do_mouseAt(event){
+    if(isMouseDown){
+        // Restore image when first clicked (rubberbanding)
+        ctx.putImageData(lastImg, 0, 0);
+        // Draw rect from orginal point to currently location
+        lastRect = { x: firstPt.x,
+                     y: firstPt.y,
+                     w: event.clientX - canvas.offsetLeft - firstPt.x,
+                     h: event.clientY - canvas.offsetTop - firstPt.y };
+        draw_aRect(null, null, lastRect);
+    }
 }
 
 // ExtraC: Add UI somewhere that give coordinates of where
@@ -74,9 +94,15 @@ function draw_GraphPaper(color, units) {
 /*
     Simple helper function to draw a stroked rectangle
 */
-function draw_aRect(color, aRect) {
+function draw_aRect(c_fill, c_stroke, aRect) {
+    ctx.save();
+
+    ctx.fillStyle = c_fill || REC_FILL_COLOR;
+    ctx.strokeStyle = c_stroke || REC_STROKE_COLOR;
     ctx.strokeRect(aRect.x, aRect.y, aRect.w, aRect.h);
     ctx.fillRect(aRect.x, aRect.y, aRect.w, aRect.h);
+
+    ctx.restore();
 }
 
 /* Some Setup functions I use to make the page look nice-er */
