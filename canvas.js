b42cdabff8d140808aafe52fc04be1c296923466
Mark Odell <dracone.cordis@gmail.com>
2014-11-25 09:47:50 -0800

    Used Jim's code to make it work :(.

--- canvas.js
+++ canvas.js
@@ -6,18 +6,21 @@ var canvas, gl;
 // Setup function for loading the page
 window.onload = function () {
   init_canvas();
-  init_Listners();
+  //init_Listners();
 
-  var nPoints = createVerticesBuffers(gl);
+  var nPoints = createVertices(gl);
   if(!nPoints){
     console.log("Unable to create points");
   }
 
-  var nIndices = createIndicesBuffers(gl);
+  var nIndices = createIndices(gl);
   if (!nIndices) {
     alert("Unable to create points.");
     return;
   }
+  
+  // Corrects depth rendering issues
+  gl.enable(gl.DEPTH_TEST);
 
   var animate = function() {
     setupView(gl);
@@ -30,37 +33,9 @@ window.onload = function () {
 
 /*--------   Functional Code   ---------*/
 // Program vars
-var POINT_SIZE = 10.0,
+var POINT_SIZE = 30.0,
     nFaces = 6,
     nVtxsFace = 4;
-
-var zehCube = new Float32Array([
-  //positions and colors interleaved
-   0.7,  0.7,  0.7,    1.0, 1.0, 1.0,
-  -0.7,  0.7,  0.7,    0.0, 1.0, 1.0,
-  -0.7, -0.7,  0.7,    0.0, 0.0, 1.0,
-   0.7, -0.7,  0.7,    1.0, 0.0, 1.0,
-  -0.7,  0.7, -0.7,    0.0, 1.0, 0.0,
-   0.7,  0.7, -0.7,    1.0, 1.0, 0.0,
-   0.7, -0.7, -0.7,    1.0, 0.0, 0.0,
-  -0.7, -0.7, -0.7,    0.0, 0.0, 0.0
-]);
-var zehCube_pWidth = 3,  // how many position coords per vertex
-    zehCube_cWidth = 3,  // how many color coords per vertex
-    zehCube_vertexWidth = zehCube_pWidth + zehCube_cWidth;
-
-var zehCube_ix = new Uint8Array([
-  0, 1, 2, 3,
-  4, 5, 6, 7,
-  5, 0, 3, 6,
-  1, 4, 7, 2,
-  5, 4, 1, 0,
-  3, 2, 7, 6
-]);
-
-// Shader vars
-var a_Position, a_PointSize, // Vertex
-    a_Color;             // Fragment
 
 var vertexShaderCode = 
     'attribute vec4 a_Position; \n' +
@@ -131,45 +106,73 @@ function setupView(gl) {
   gl.uniformMatrix4fv(u_MVPMatrix, false, mvpMatrix);
 }
 
-function createVerticesBuffers(gl, vxs){
-
-  // Create a vertex buffer
-  var vBuffer = gl.createBuffer();
-  if(!vBuffer) {
-    console.log("Cannot create vertex buffer but carrying on.\n");
-  }
-
-  // Binding buffer sets it to be used in next draw?
-  // Once a buffer is bound and cannot be bound again.
-  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, zehCube, gl.STATIC_DRAW);
-
-  var fltsize = zehCube.BYTES_PER_ELEMENT;
-  var stride         = zehCube_vertexWidth * fltsize,
-      positionOffset = 0, //* fltfize,
-      colorOffset    = zehCube_pWidth * fltsize;
+function createVertices(gl) {
   
-  // How to draw vertices
-  a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  gl.vertexAttribPointer(a_Position, zehCube_pWidth, gl.FLOAT, false, 
+  // define a block of binary data interpreted as Float32Array
+  
+  var vertexPositionsColors = new Float32Array([
+    //positions and colors interleaved
+     0.7,  0.7,  0.7,    1.0, 1.0, 1.0,
+    -0.7,  0.7,  0.7,    0.0, 1.0, 1.0,
+    -0.7, -0.7,  0.7,    0.0, 0.0, 1.0,
+     0.7, -0.7,  0.7,    1.0, 0.0, 1.0,
+    -0.7,  0.7, -0.7,    0.0, 1.0, 0.0,
+     0.7,  0.7, -0.7,    1.0, 1.0, 0.0,
+     0.7, -0.7, -0.7,    1.0, 0.0, 0.0,
+    -0.7, -0.7, -0.7,    0.0, 0.0, 0.0
+  ]);
+
+  var pCoords = 3,  // how many position coords per vertex
+      cCoords = 3,  // how many color coords per vertex
+      vertexWidth = pCoords + cCoords;
+
+  var floatSize = vertexPositionsColors.BYTES_PER_ELEMENT;
+  var stride = vertexWidth * floatSize,   // how many bytes per vertex
+      positionOffset = 0 * floatSize,     // starting byte of positions
+      colorOffset = pCoords * floatSize;  // starting byte of colors
+  
+  // create a buffer for the vertex data
+  var vBuffer = gl.createBuffer();
+  if (!vBuffer) {
+    alert("Unable to create vertex buffer.");
+    return 0;
+  }
+  
+  // make vBuffer the "current" buffer and copy data into it
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertexPositionsColors, gl.STATIC_DRAW);
+  
+  // tell the vertex shader how to determine positions
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  gl.vertexAttribPointer(a_Position, pCoords, gl.FLOAT, false, 
                          stride, positionOffset);
   gl.enableVertexAttribArray(a_Position);
 
-  // How to draw Color Data
-  a_Color = gl.getUniformLocation(gl.program, 'a_Color');
-  gl.vertexAttribPointer(a_Color, zehCube_cWidth, gl.FLOAT, false,
+  // tell the vertex shader how to determine colors
+  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+  gl.vertexAttribPointer(a_Color, cCoords, gl.FLOAT, false,
                          stride, colorOffset);
   gl.enableVertexAttribArray(a_Color);
+  
+  // tell the vertex shader about the point size
+  var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+  gl.vertexAttrib1f(a_PointSize, 10.0);
 
-  // Size of the vertices drawn
-  a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
-  gl.vertexAttrib1f(a_PointSize, POINT_SIZE);
-
-  // Numbah of vertices
-  return zehCube.length / zehCube_vertexWidth;
+  // return the number of vertices
+  return vertexPositionsColors.length / vertexWidth;
 }
 
-function createIndicesBuffers(gl, ins){
+function createIndices(gl) {
+  var indices = new Uint8Array([
+    0, 1, 2, 3,
+    4, 5, 6, 7,
+    5, 0, 3, 6,
+    1, 4, 7, 2,
+    5, 4, 1, 0,
+    3, 2, 7, 6
+  ]);
+  
+  // create a buffer for the indices
   var iBuffer = gl.createBuffer();
   if (!iBuffer) {
     alert("Unable to create index buffer.");
@@ -178,83 +181,45 @@ function createIndicesBuffers(gl, ins){
   
   // make iBuffer the "current" buffer and copy data into it
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, zehCube_ix, gl.STATIC_DRAW);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
   
-  // return the number of vertices (as in the array of indices)
-  return zehCube_ix.length;
+  // return the number of vertices
+  return indices.length;
 }
 
 function draw_Frame(gl, nPoints, nFaces, nVtxsFace) {
+  
   // Clear and draw the points
-  gl.clearColor(0.2, 0.2, 0.2, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clearColor(0.1, 0.1, 0.1, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.drawArrays(gl.POINTS, 0, nPoints);
 
     // draw the face loops
   for (var face = 0; face < nFaces; face++) {
-    gl.drawElements(gl.LINE_LOOP, nVtxsFace, gl.UNSIGNED_BYTE, face * nVtxsFace);
+    gl.drawElements(gl.TRIANGLE_FAN, nVtxsFace, gl.UNSIGNED_BYTE, face * nVtxsFace);
   } 
 }
 
-function init_Listners() {
-  canvas.addEventListener("mousedown", do_MouseDown);
-  canvas.addEventListener("mouseup", do_MouseUp);
-  canvas.addEventListener("mousemove", do_MouseMove);
-  document.getElementById("erase").onclick = eraseAll;
-} 
-// vvv Listeners vvv \\
-function do_MouseDown(event) {
-
-}
-
-function do_MouseMove(event) {
-
-}
-
-function do_MouseUp(event) {
-  
-}
-// ^^^ Listeners ^^^ \\
-
-
 /*-------- END Functional Code ---------*/
-
-function randomColor() {
-  var aClr = Math.random();
-  console.log(aClr);
-  return aClr;
-}
-
-function getPtMouseEvent(event) {
-  var mouseX = event.clientX - canvas.offsetLeft,
-      mouseY = event.clientY - canvas.offsetTop;
-  
-  var glPt = {x:(mouseX - canvas.width/2) / (canvas.width/2), 
-              y:(canvas.height/2 - mouseY) / (canvas.height/2)};
-  return glPt;
-}
-
-function eraseAll() {
-  gl.clear(gl.COLOR_BUFFER_BIT);
-}
 
 /*--   Init functions for Canvas   --*/
 
 function init_canvas(){
-    canvas = document.getElementById("canvas");
-    canvas.width = window.innerWidth - 20;
-    canvas.height = window.innerHeight - 20;
-    gl = canvas.getContext('webgl');
-    if(!gl) {
-        alert('Canvas could not be found by ID:' + canvas);
-        return;
-    }
+  canvas = document.getElementById("canvas");
+  var d = 
+  (window.innerWidth > window.innerHeight) ? window.innerHeight : window.innerWidth; 
+  canvas.width = canvas.height = d - 20;
+  gl = canvas.getContext('webgl');
+  if(!gl) {
+      alert('Canvas could not be found by ID:' + canvas);
+      return;
+  }
 
-    // initialize the shaders
-    if (!initShaders(gl, vertexShaderCode, fragmentShaderCode)) {
-        alert('Cannot initialize the shaders.');
-        return;
-    }
+  // initialize the shaders
+  if (!initShaders(gl, vertexShaderCode, fragmentShaderCode)) {
+      alert('Cannot initialize the shaders.');
+      return;
+  }
 }
 /*-- END Init functions for Canvas --*/
 
